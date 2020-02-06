4453294f0a4e2da35552b294828eb132de5ebbc6
mark-summerfield <mark@qtrac.eu>
2020-02-06 16:06:10 +0000

    more progress with help from Steven Schveighoffer

--- src/package.d
+++ src/package.d
@@ -1,6 +1,8 @@
 // Copyright © 2020 Mark Summerfield. All rights reserved.
 module diffrange;
 
+import std.range: ElementType, front, isForwardRange;
+
 struct Match {
     size_t aStart;
     size_t bStart;
@@ -22,7 +24,12 @@ struct Span {
     size_t bEnd;
 }
 
-class Diff(T, E) {
+class Diff(T) if (
+        isForwardRange!T && // T is a range
+        is(typeof(T.init.front == T.init.front)) // Elements support ==
+        ) {
+    alias E = ElementType!T;
+
     T a;
     T b;
     size_t[][E] b2j;
@@ -33,20 +40,24 @@ class Diff(T, E) {
         chainB();
     }
 
-    void chainB() {
+    private final void chainB() {
         foreach (i, element; b)
             b2j[element] ~= i;
         // TODO
     }
 }
 
+auto differ(T)(T a, T b) { return Diff!T(a, b); }
+
 unittest {
+    import std.array;
     import std.stdio: writeln;
 
     writeln("unittest for the diffrange library.");
+    auto d1 = differ("one two three four".array, "one too tree four".array);
     auto a = ["Tulips are yellow,", "Violets are blue,", "Agar is sweet,",
               "As are you."];
     auto b = ["Roses are red,", "Violets are blue,", "Sugar is sweet,",
               "And so are you."];
-    auto diff = Diff(a, b);
+    auto d2 = differ(a, b);
 }
