3e441f2758411ad2e242dc2aff82ae44b3b40e0c
mark-summerfield <mark@qtrac.eu>
2020-02-08 03:41:11 +0000

    renamed etc.
    diffrange → diffslice since that's more accurate and won't mislead into
    users thinking it is incremental like a range

--- src/package.d
+++ src/package.d
@@ -1,5 +1,5 @@
 // Copyright © 2020 Mark Summerfield. All rights reserved.
-module diffrange;
+module diffslice;
 
 import std.range: ElementType, front, isForwardRange;
 
@@ -24,20 +24,20 @@ struct Span {
     size_t bEnd;
 }
 
-class Diff(T) if (
-        isForwardRange!T && // T is a range
-        is(typeof(T.init.front == T.init.front)) // Elements support ==
+private class Diff(R) if (
+        isForwardRange!R && // R is a range that can be iterated repeatedly
+        is(typeof(R.init.front == R.init.front)) // Elements support ==
         ) {
     import std.conv: to;
     import std.math: floor;
 
-    alias E = ElementType!T;
+    alias E = ElementType!R;
 
-    T a;
-    T b;
-    size_t[][E] b2j;
+    private R a;
+    private R b;
+    private size_t[][E] b2j;
 
-    this(T a, T b) {
+    private this(R a, R b) {
         this.a = a;
         this.b = b;
         chainB();
@@ -63,27 +63,52 @@ class Diff(T) if (
         immutable bLen = b.length;
         size_t[] queue; // =[(0, aLen, 0, bLen)] i.e., list of 1 x 4-tuple
         Match[] matches;
-
+        // TODO
         return matches;
+    }
+
+    final Span[] spans() {
+        Span[] spans;
+        // TODO
+        return spans;
     }
 }
 
-auto differ(T)(T a, T b) if (
-        isForwardRange!T && // T is a range
-        is(typeof(T.init.front == T.init.front)) // Elements support ==
+auto matches(R)(R a, R b) if (
+        isForwardRange!R && // R is a range that can be iterated repeatedly
+        is(typeof(R.init.front == R.init.front)) // Elements support ==
         ) {
-    return new Diff!T(a, b);
+    auto diff = new Diff!R(a, b);
+    return diff.matches();
+}
+
+auto spans(R)(R a, R b) if (
+        isForwardRange!R && // R is a range that can be iterated repeatedly
+        is(typeof(R.init.front == R.init.front)) // Elements support ==
+        ) {
+    auto diff = new Diff!R(a, b);
+    return diff.spans();
 }
 
 unittest {
     import std.array;
     import std.stdio: writeln;
 
-    writeln("unittest for the diffrange library.");
-    auto d1 = differ("one two three four".array, "one too tree four".array);
-    auto a = ["Tulips are yellow,", "Violets are blue,", "Agar is sweet,",
-              "As are you."];
-    auto b = ["Roses are red,", "Violets are blue,", "Sugar is sweet,",
-              "And so are you."];
-    auto d2 = differ(a, b);
+    writeln("unittest for the diffslice library.");
+
+    auto a1 = "one two three four";
+    auto b1 = "one too tree four";
+    auto m1 = matches(a1.array, b1.array);
+    writeln("TODO matches", m1);
+    auto s1 = spans(a1.array, b1.array);
+    writeln("TODO spans", s1);
+
+    auto a2 = ["Tulips are yellow,", "Violets are blue,", "Agar is sweet,",
+               "As are you."];
+    auto b2 = ["Roses are red,", "Violets are blue,", "Sugar is sweet,",
+               "And so are you."];
+    auto m2 = matches(a2, b2);
+    writeln("TODO matches", m2);
+    auto s2 = spans(a2, b2);
+    writeln("TODO spans", s2);
 }
