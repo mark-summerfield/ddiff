ae92ba051dba3f4eed3eaad32b3dc944b26ee6a0
mark-summerfield <mark@qtrac.eu>
2020-02-08 08:59:38 +0000

    started on matches()

--- src/package.d
+++ src/package.d
@@ -1,7 +1,9 @@
 // Copyright © 2020 Mark Summerfield. All rights reserved.
 module ddiff;
 
+import std.container.rbtree: RedBlackTree;
 import std.range: ElementType, front, isForwardRange;
+import std.typecons: Tuple;
 
 struct Span {
     Tag tag;
@@ -19,6 +21,9 @@ enum Tag: string {
     Delete = "delete",
     Replace = "replace",
 }
+
+private alias Indexes = Tuple!(size_t, "aStart", size_t, "aEnd",
+                               size_t, "bStart", size_t, "bEnd");
 
 private struct Match {
     size_t aStart;
@@ -48,25 +53,51 @@ private class Diff(R) if (
     private final void chainB() {
         foreach (i, element; b)
             b2j[element] ~= i;
-        int[E] popular; // key = element, value = 0 (used as a set)
+        auto popular = new RedBlackTree!E;
         auto len = b.length;
         if (len > 200) {
             auto popularLen = to!int(floor((to!double(len) / 100.0))) + 1;
             foreach (element, indexes; b2j)
                 if (indexes.length > popularLen)
-                    popular[element] = 0;
-            foreach (element; popular.byKey)
+                    popular.insert(element);
+            foreach (element; popular)
                 b2j.remove(element);
         }
     }
 
     private final Match[] matches() {
+        import std.array: back, empty;
+        import std.range.primitives: popBack;
+
         immutable aLen = a.length;
         immutable bLen = b.length;
-        size_t[] queue; // =[(0, aLen, 0, bLen)] i.e., list of 1 x 4-tuple
+        Indexes[] indexQueue = [Indexes(0, aLen, 0, bLen)];
         Match[] matches;
+        while (!indexQueue.empty) {
+            auto indexes = indexQueue.back();
+            indexQueue.popBack();
+            auto match = longestMatch(indexes);
+            auto i = match.aStart;
+            auto j = match.bStart;
+            auto k = match.length;
+            if (k > 0) {
+                matches ~= match;
+                if (indexes.aStart < i && indexes.bStart < j)
+                    indexQueue ~= Indexes(indexes.aStart, i,
+                                          indexes.bStart, j);
+                if (i + k < indexes.aEnd && j + k < indexes.bEnd)
+                    indexQueue ~= Indexes(i + k, indexes.aEnd,
+                                          j + k, indexes.bEnd);
+            }
+        }
         // TODO
         return matches;
+    }
+
+    private final Match longestMatch(Indexes indexes) {
+        Match match;
+
+        return match;
     }
 
     final Span[] spans(EmptySpan emptySpan) {
